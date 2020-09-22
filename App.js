30818827912f8c4febcfe3ea5614f40a90fbd989
Roberto Ross <dev@rros.me>
2020-09-22 00:54:53 -0500

    Initial commit: Environment setup.

---
+++ App.js
@@ -0,0 +1,34 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+function App() {
+  return(
+    <View style={styles.container}>
+      <Text>Testing</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'blue'
+  }
+});
+
+export default App;
---
+++ app/components/Input.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { View, Text, TextInput } from 'react-native';
+
+function Input() {
+  return (
+    <View></View>
+  );
+}
